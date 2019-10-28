e50887f7eec4f5f9b23b1888e433d4a6c02938a5
Ben Whitfield <ben.whitfield@ANSGRP-002884.local>
2019-10-28 10:06:39 +0000

    add get and createLink functionality

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+
+import '../styles/App.css';
+
+function App() {
+  return (
+    <CreateLink />
+
+  );
+}
+
+export default App;
